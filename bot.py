172a2c5f46aa0d6ebf3cd37ef9b3a919525ea30a
Rayyan Zamir <Rayyan@macairs-air.lan>
2020-09-16 08:12:56 -0500

    new quotes

--- bot.py
+++ bot.py
@@ -26,12 +26,19 @@ async def holmeswisdom(ctx):
     wisdom = [
         "You are so weird",
         "Like, some kind of Jesus figure?",
-        "I look intimidating but I'm really not",
-        "I hope there are no more reptiles during AP testing",
+        "I look intimidating but I'm really not.",
+        "I hope there are no more reptiles during AP testing.",
         "Any questions, comments, concerns?",
-        "I think eels are fascinating",
-        "Every year we have at least one kid fail their diploma",
-        "What did I just walk into. Is this a bad time"
+        "I think eels are fascinating.",
+        "Every year we have at least one kid fail their diploma.",
+        "What did I just walk into? Is this a bad time?",
+        "Should I be concerned?",
+        "Terrifying.",
+        "She could rip my face off and I would still love her.",
+        "I have no idea what you are saying.",
+        "Someone once described caviar to me as having the same consistency as bath oil beads.",
+        "SO GLAD to hear you’ve been floor swimming.",
+        "Real as penitentiary steel.",
     ]
     response = random.choice(wisdom)
     await ctx.send(response)
@@ -47,7 +54,11 @@ async def karenwisdom(ctx):
         "Fabulous discussion",
         "Could you slide that off your desk please?",
         "You've got some good stuff!",
-        "And you know, that's a really fascinating point you bring up. It reminds me of [some theory I read the other day]"
+        "And you know, that's a really fascinating point you bring up. It reminds me of [some theory I read the other day]",
+        "Hello my friends!",
+        "We see the homeless as two standard deviations below the mean of human.",
+        "Could you unplug for me please?",
+
     ]
     kresponse = random.choice(kwisdom)
     await ctx.send(kresponse)
@@ -56,20 +67,27 @@ async def karenwisdom(ctx):
 @bot.command(name='susanwisdom', help='Quotes from Elledge herself')
 async def susanwisdom(ctx):
     swisdom = [
-        "So whatever on that one",
+        "So whatever on that one.",
         "And y'all...",
         "Hitler was not democratically elected.",
         "The Lusitania did not cause the United States to enter WWI.",
+        "I like the First Moroccan Crisis.",
+        "Someone talk other than Huy",
+        "I like Truman.",
+        "Y’all take out your notes.",
+        "MacArthur was a...questionable kinda guy.",
+
     ]
     sresponse = random.choice(swisdom)
     await ctx.send(sresponse)
 
 @bot.command(name='rayyanwisdom', help='...no explanation necessary')
 async def rayyanwisdom(ctx):
-    wisdom = [
+    rwisdom = [
         "fuck you",
+        "women don't deserve rights",
     ]
-    response = random.choice(wisdom)
-    await ctx.send(response)
+    rresponse = random.choice(rwisdom)
+    await ctx.send(rresponse)
 
 bot.run(TOKEN)
